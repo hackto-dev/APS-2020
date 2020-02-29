58bb905257157bb6a4c7618ff2a5d44e6b7e1918
Prathamesh Kulkarni <prathddon@gmail.com>
2020-02-29 11:36:39 +0530

    Create union-find.cpp
    Added union-find

---
+++ union-find.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define int long long int
+#define fi first
+#define se second
+#define pub push_back
+#define mkp make_pair
+#define pi pair<int,int>
+#define push push
+#define all(v) v.begin(),v.end()
+#define rep(i,l,r) for(int i = (int)(l) ; i < (int)(r) ; i++)
+#define clr(a,x) memset(a,x,sizeof(a));
+#define rr(v) for(auto &val:v)
+#define print(v) for(const auto itr:v){ cout << itr << ' ';} cout << endl;
+#define init(v,x) for(auto &itr:v){ itr = x; }
+#define minpq(x) x,vector<x>,greater<x>
+#define ln length()
+#define sz size()
+#define infinity 1e18;
+#define elif else if
+
+int arr[100000];
+int size[100000];
+
+void addEdge(vector<pi> adj[],int u,int v,int wt) {
+    adj[u].pub(mkp(v,wt));
+    adj[v].pub(mkp(u,wt));
+}
+
+int root(int i) {
+    while(arr[i] != i) {
+        i = arr[i];
+    }
+    
+    return i;
+}
+
+bool find(int u,int v) {
+    if(root(u) == root(v)) {
+        return true;
+    }
+    
+    else {
+        return false;
+    }
+}
+
+void initialize(int n) {
+    rep(i,0,n) {
+        arr[i] = i;
+        size[i] = 1;
+    }
+}
+
+void union(int A, int B)
+{
+    int TEMP = arr[A];
+    for(int i = 0; i < N;i++) {
+        if(arr[i] == TEMP)
+            arr[i] = arr[B]; 
+    }
+    
+    rep(i,0,n)
+        cout << arr[i] << " ";
+        
+    cout << endl;
+}
+
+int32_t main(){
+    fastIO
+    
+    int n;
+    cin >> n;
+    
+    initialize(n);
+    
+    int u,v;
+    cin >> u >> v;
+    
+    weighted_union(u,v);
+
+    return 0;
+}
