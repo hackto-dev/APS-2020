365fcb951143e8cfeab8d03d22854cd3893398af
hackto-dev <pratham.kulkarni1999@gmail.com>
2020-03-27 21:34:06 +0530

    Added code for catalan numbers

---
+++ catalan.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define int long long int
+#define fi first
+#define se second
+#define pub push_back
+#define mkp make_pair
+#define pi pair<int, int>
+#define push push
+#define all(v) v.begin(), v.end()
+#define rep(i, l, r) for (int i = (int)(l); i < (int)(r); i++)
+#define clr(a, x) memset(a, x, sizeof(a));
+#define rr(v) for (auto &val : v)
+#define print(v)             \
+    for (const auto itr : v) \
+    {                        \
+        cout << itr << ' ';  \
+    }                        \
+    cout << endl;
+#define init(v, x)      \
+    for (auto &itr : v) \
+    {                   \
+        itr = x;        \
+    }
+#define minpq(x) x, vector<x>, greater<x>
+#define ln length()
+#define sz size()
+#define infinity 1e18;
+#define elif else if
+#define mod 1e9;
+
+unsigned int catalan(unsigned int n) {
+    if(n <= 1)
+        return 1;
+
+    unsigned int res = 0;
+
+    rep(i,0,n)
+        res += catalan(i)*catalan(n-i-1);
+
+    return res;
+}
+
+int32_t main()
+{
+    fastIO
+
+    int n;
+
+    cin >> n;
+
+    cout << catalan(n) << endl;
+
+    return 0;
+}
