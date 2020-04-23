edb9e1f0929b899f2608c3a69a3f5c7cdb38ef76
hackto-dev <pratham.kulkarni1999@gmail.com>
2020-04-23 16:09:14 +0530

    Added files via upload

---
+++ primes.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define int long long int
+#define fi first
+#define se second
+#define pub push_back
+#define mkp make_pair
+#define pi pair<int, int>
+#define push push
+#define all(v) v.begin(), v.end()
+#define rep(i, l, r) for (int i = (int)(l); i < (int)(r); i++)
+#define clr(a, x) memset(a, x, sizeof(a));
+#define rr(v) for (auto &val : v)
+#define print(v)             \
+    for (const auto itr : v) \
+    {                        \
+        cout << itr << ' ';  \
+    }                        \
+    cout << endl;
+#define init(v, x)      \
+    for (auto &itr : v) \
+    {                   \
+        itr = x;        \
+    }
+#define minpq(x) x, vector<x>, greater<x>
+#define ln length()
+#define sz size()
+#define infinity 1e18;
+#define elif else if
+#define mod 1e9;
+
+int32_t main()
+{
+    fastIO
+
+    int start,end,i,j,flag = 0;
+
+    cin >> start >> end;
+
+    rep(i,start,end+1) {
+        flag = 0;
+
+        rep(j,2,i) {
+            if(i%j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag == 0)
+            cout << i << " ";
+    }
+
+    cout << endl;
+    
+    return 0;
+}
